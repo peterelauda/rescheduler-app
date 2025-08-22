90470f7606d547df1162129037d197002767ee79
peterelauda <peterelauda>
2025-08-22 12:55:10 +0800

    second commit

--- app/Console/Commands/CustomCommand.php
+++ app/Console/Commands/CustomCommand.php
@@ -4,6 +4,10 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class CustomCommand extends Command
 {
@@ -26,6 +30,31 @@ class CustomCommand extends Command
      */
     public function handle()
     {
-        Log::info("Hello world!");;
+        Log::info("Hello world!");
+        $faker = Faker::create();
+
+        $name = $faker->name;
+        $email = $faker->unique()->safeEmail;
+        $passwordPlain = Str::random(10);
+        $passwordHash = Hash::make($passwordPlain);
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $passwordHash,
+        ]);
+
+        if ($this->output) {
+            $this->info("User berhasil dibuat:");
+            $this->info("Name: $name");
+            $this->info("Email: $email");
+            $this->info("Password: $passwordPlain (hash tersimpan di DB)");
+        } else {
+            Log::info("User berhasil dibuat:");
+            Log::info("Name: $name");
+            Log::info("Email: $email");
+            Log::info("Password: $passwordPlain (hash tersimpan di DB)");
+        }
+
     }
 }
